6f8d992d6b11bcb5c5592036387970c6ab2c2e02
N. A. Ronald <nronald@swin.edu.au>
2020-01-30 11:59:04 +1100

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// 1.  Write a function that adds two numbers together.
+
+
+// 2. Complete the following class with an age property.
+
+class Person {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
+
+let p = Person(name: "me")
+print(p.name)
